1c5f52ee412152f8052beaab1bbbe0fab621ddae
Rohit0924 <rohitsg2609@gmail.com>
2026-05-08 22:52:51 +0530

    Fix API routes

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,22 @@ const NoteSchema = new mongoose.Schema({
 
 const Note = mongoose.model("Note", NoteSchema);
 
+app.get("/api/notes", async (req, res) => {
+  const notes = await Note.find();
+  res.json(notes);
+});
+
+app.post("/api/notes", async (req, res) => {
+
+  const note = new Note({
+    text: req.body.text,
+  });
+
+  await note.save();
+
+  res.json(note);
+});
+
 app.listen(5000, () => {
   console.log("Backend Running");
 });
